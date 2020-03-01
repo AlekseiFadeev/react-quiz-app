4abd3f39bff1a76376f9a48b377766449fbbaac2
Aleksei Fadeev <33324706+AlekseiFadeev@users.noreply.github.com>
2020-03-01 16:47:35 +0300

    login page to redux

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,9 +3,10 @@ import classes from "./Auth.module.css";
 import Button from "../../components/UI/Button/Button";
 import Input from "../../components/UI/Input/Input";
 import is from "is_js";
-import Axios from "axios";
+import { connect } from "react-redux";
+import { auth } from "../../store/actions/auth";
 
-export default class Auth extends Component {
+class Auth extends Component {
 
   state = {
     isFormValid: false,
@@ -37,36 +38,20 @@ export default class Auth extends Component {
     }
   }
 
-  loginHandler = async () => {
-    const authData= {
-      email: this.state.formControls.email.value,
-      password: this.state.formControls.password.value,
-      returnSecureToken: true
-    }
-
-    try{
-      const response = await Axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCS7WjVtJ3_hhEGplStmf3p9uPtajx00Lw', authData)
-      console.log(response.data)
-    }catch (e) {
-      console.log(e)
-    }
-
+  loginHandler = () => {
+    this.props.auth(
+      this.state.formControls.email.value,
+      this.state.formControls.password.value,
+      true
+    )
   }
 
-  registerHandler = async () => {
-    const authData= {
-      email: this.state.formControls.email.value,
-      password: this.state.formControls.password.value,
-      returnSecureToken: true
-    }
-
-    try{
-      const response = await Axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCS7WjVtJ3_hhEGplStmf3p9uPtajx00Lw', authData)
-      console.log(response.data)
-    }catch (e) {
-      console.log(e)
-    }
-
+  registerHandler = () => {
+    this.props.auth(
+      this.state.formControls.email.value,
+      this.state.formControls.password.value,
+      false
+    )
   }
 
   submitHandler = event => {
@@ -165,3 +150,11 @@ export default class Auth extends Component {
     )
   }
 }
+
+function mapDispathToProps(dispatch) {
+  return {
+    auth: (email, password, isLogin) => dispatch(auth(email, password, isLogin))
+  }
+}
+
+export default connect(null, mapDispathToProps)(Auth)
---
+++ src/store/actions/auth.js
@@ -0,0 +1,54 @@
+import Axios from "axios";
+import {AUTH_SUCCESS, AUTH_LOGOUT} from "./actionTypes"
+
+export function auth(email, password, isLogin) {
+    return async dispath => {
+
+        const authData= {
+          email, password,
+          returnSecureToken: true
+        }
+
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCS7WjVtJ3_hhEGplStmf3p9uPtajx00Lw'
+
+        if (isLogin) {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCS7WjVtJ3_hhEGplStmf3p9uPtajx00Lw';
+        }
+
+        const response = await Axios.post(url, authData)
+        const data = response.data
+
+        const expirationDate = new Date(new Date().getTime() + data.expiresIn * 1000)
+
+        localStorage.setItem('token', data.idToken)
+        localStorage.setItem('userid', data.localId)
+        localStorage.setItem('expirationDate', expirationDate)
+
+        dispath(authSuccess(data.idToken))
+        dispath(autoLogout(data.expiresIn))
+    }
+}
+
+export function autoLogout(time) {
+    return dispath => {
+        setTimeout(() => {
+            dispath(loguot())
+        }, time * 1000)
+    }
+}
+
+export function loguot() {
+    localStorage.removeItem('token')
+    localStorage.removeItem('userid')
+    localStorage.removeItem('expirationDate')
+    return {
+        type: AUTH_LOGOUT
+    }
+}
+
+export function authSuccess(token) {
+    return {
+        type: AUTH_SUCCESS,
+        token
+    }
+}
